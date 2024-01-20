05f13c7ddacacc799bf7754660e3f65c66310b52
Caio <bukvar2009@gmail.com>
2024-01-20 16:20:39 -0300

    moving use server

--- src/app/api/database/updateUser/route.ts
+++ src/app/api/database/updateUser/route.ts
@@ -1,8 +1,8 @@
-"use server";
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(req: NextRequest, res: NextResponse) {
+  "use server";
   try {
     const body = await req.json();
     const { id, points } = body;
